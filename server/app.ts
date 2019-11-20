5d4c409a85e9d923d037093eb71d16813378f0f0
sanjana_ki_dukaan <sanjana.sahoo@mtap.in>
2019-11-20 11:18:52 +0530

    added logging files

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,41 @@
-import Express, { Application } from 'express'
+import express from 'express'
+//import Express, { Application } from 'express'
+import accessLogStream from '../logger/logging'
 import { config } from 'dotenv'
 import { appRouter as apiRouer } from './routes'
+//import { createLogger, transports ,format, level, info}from 'winston';
+var morgan = require('morgan')
+// ... Then later, the new code
+//import expressWinston, { LoggerOptions } from 'express-winston';
+const app = express()
+// const loggerConfig: LoggerOptions = logger.info('Informing.');
+// app.use(expressWinston.logger({
+
+//     transports: [
+//      // new (transports.Console)(),
+//       new transports.File({filename:`logs/%DATE%.log`,level:'info'})
+      
+//     ]
+//   }));
+// app.use(expressWinston.logger(loggerConfig));
+
+
 config()
 
-const app: Application = Express()
 
-app.use(Express.static('public'))
-app.use('/api', apiRouer)
-// app.get('*', (req, res) => res.send('not found'))
+app.use(express.static('public'))
+app.use(morgan('combined', { stream: accessLogStream }))
+ 
+app.use('/', apiRouer)
 const port: number | string = process.env.PORT || 3000
 
 app.listen(port, () => {
     console.info(`listening on port ${port}`)
 })
+            /* Some code */
+        
+// app.get('*', (req, res) => res.send('not found'))
+
+//app.listen(port, () => {
+    //console.info(`listening on port ${port}`)
+//})
